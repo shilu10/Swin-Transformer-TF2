7e3381308384c4b07c062dec9a4bedc833c7d7f1
adminuser <adminuser@kali.mvm>
2023-07-21 19:13:15 +0530

    included the base config file

--- base_config.py
+++ base_config.py
@@ -1,5 +1,15 @@
 from ml_collections import ConfigDict 
+from typing import * 
 
-
-def get_base_config():
+def get_base_config(image_size: int = 224, 
+                    patch_size: int = 4,
+                    projection_dim: int = 96,
+                    window_size: int = 7,
+                    depths: List = [2, 2, 6, 2],
+                    num_heads: List = [3, 6, 12, 24],
+                    drop_rate: float = 0.0, 
+                    drop_path_rate: float = 0.0, 
+                    
+                    
+            ):
     pass 

fafb11dd0e30bf73f2322fe593004cd91215c120
adminuser <adminuser@kali.mvm>
2023-07-21 19:18:35 +0530

    updated the base_config.py

--- base_config.py
+++ base_config.py
@@ -9,7 +9,28 @@ def get_base_config(image_size: int = 224,
                     num_heads: List = [3, 6, 12, 24],
                     drop_rate: float = 0.0, 
                     drop_path_rate: float = 0.0, 
-                    
-                    
-            ):
-    pass 
+                    num_classes: int = 1000,
+                    include_top: bool = True,
+                    attn_drop_rate: float = 0.0,
+                ):
+
+    config = ConfigDict() 
+    config.image_size = image_size
+    config.patch_size = patch_size
+    config.projection_dim = projection_dim
+    config.window_size = window_size 
+    config.depths = depths
+    config.num_heads = num_heads
+    config.num_classes = num_classes
+    config.drop_rate = drop_rate 
+    config.drop_path_rate = drop_path_rate
+    config.attn_drop_rate = attn_drop_rate
+    config.include_top = include_top
+
+    # common configs
+    config.initializer_range = 0.1
+    config.patch_resolution = (config.image_size[0] // config.patch_size, config.image_size[1] // config.patch_size)
+    config.act_layer = 'gelu'
+    config.norm_layer = 'layer_norm'
+
+    return config.lock()

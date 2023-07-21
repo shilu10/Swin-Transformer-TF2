a15c7539298f0d5facf12707d8dbd3d769273ca9
adminuser <adminuser@kali.mvm>
2023-07-21 19:09:38 +0530

    included the base config file

---
+++ base_config.py
@@ -0,0 +1,5 @@
+from ml_collections import ConfigDict 
+
+
+def get_base_config():
+    pass 
--- swin_transformer/blocks/utils.py
+++ swin_transformer/blocks/utils.py
@@ -1,5 +1,5 @@
 import tensorflow as tf 
-from tensorflow import numpy 
+from tensorflow import keras 
 import numpy as np 
 
 

b1f9eb8c3e6ae09a9744a85428da0b43842337a9
adminuser <adminuser@kali.mvm>
2023-07-23 14:17:51 +0530

    updated the config files and port_all code

--- covert_all.py
+++ covert_all.py
@@ -4,24 +4,26 @@ import glob
 import numpy as np 
 import yaml 
 from imutils import paths 
-from porting import port_weights
+from .convert import port_weights
 
 # all config files 
-
 def main():
 
     try:
         config_file_paths = list(paths.list_files("configs/"))
         for config_file_path in config_file_paths:
             # porting all model types from pytorch to tensorflow
+            try:
+                model_type = config_file_path.split("/")[-1].split(".")[0]
+                print(f"Processing the  model type: {model_type}")
 
-            model_type = config_file_path.split("/")[-1].split(".")[0]
-            print(f"Processing the  model type: {model_type}")
-
-            port_weights(
-                model_type=model_type
-                include_top=True
-            )    
+                port_weights(
+                    model_type=model_type
+                    include_top=True
+                )    
+            
+            except Exception as err:
+                print("This specific model_type cannot be ported", err)
 
     except Exception as err:
         return err
